d94cc27ed1fed17614c70c24444aee282102a293
조현빈 <whgusqls5840@gmail.com>
2023-07-10 04:10:13 +0000

    움

--- Day_1-2/HelloWorld.py
+++ Day_1-2/HelloWorld.py
@@ -1,1 +1,7 @@
+#주석
+
 print("helloworld")
+print('helloworld2')
+print("큰따안에'-작따'")
+
+
---
+++ Day_1-2/HelloWorld_EX1.py
@@ -0,0 +1,3 @@
+#문제: 경로 "C:\Download\'hello    world'.py"출력하기
+
+print("\"C:\Download\\'hello\tworld'.py\"")
