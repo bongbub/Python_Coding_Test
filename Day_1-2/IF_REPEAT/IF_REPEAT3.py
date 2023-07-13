048509ab4ef21a403457bf8eb81071c2f2a5e6d1
조현빈 <whgusqls5840@gmail.com>
2023-07-13 02:17:34 +0000

    Repeat 수정

--- Day_1-2/IF_REPEAT/IF_REPEAT3.py
+++ Day_1-2/IF_REPEAT/IF_REPEAT3.py
@@ -1,5 +1,23 @@
-#day 1
+# DAY1 - 조건문, 반복문
 
-#조건문 / 반복문
+n = int(input('정수 1개 입력 받아 분류하기 :'))
+if n < 0 :
+    if n % 2 == 0 :
+        print('음수, 짝수 :A')
+    else:
+        print('음수, 홀수 :B')
+else:
+    if n % 2 == 0:
+        print('양수, 짝수 :C')
+    else:
+        print('양수, 홀수 :D')
+        
+if a>=90:
+    print("A학점") 
+elif a>=70: # elif는 else if의 약어, 들여쓰기 없어 가독성 굿
+    print("B학점")
+elif a>=40:
+    print("C학점")
+else:
+    print("D학점")
 
---> 이것도 써야댐............................
